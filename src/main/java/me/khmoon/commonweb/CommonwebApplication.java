2218fdfde7cb890d92e9ab1310cfa8d3f8ddac48
kwanghyunMoon <mgh3326@naver.com>
2019-07-24 16:42:09 +0900

    first update

---
+++ src/main/java/me/khmoon/commonweb/CommonwebApplication.java
@@ -0,0 +1,13 @@
+package me.khmoon.commonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonwebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommonwebApplication.class, args);
+  }
+
+}
